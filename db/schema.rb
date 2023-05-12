d2ef5f7d5246a5d406b0cdca54614b9666b70daf
Mario Gonzalez <mario.gonzalez@kellogg.northwestern.edu>
2023-05-12 21:19:25 +0000

    class 7 exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_223236) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_11_223236) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "city"
     t.string "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -26,16 +25,16 @@ ActiveRecord::Schema.define(version: 2022_02_11_223236) do
     t.string "email"
     t.string "phone_number"
     t.integer "company_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
     t.string "body"
     t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
